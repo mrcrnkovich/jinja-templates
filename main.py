e2eff9c024e33ed95cb62ccab091eea7f9149f81
mrcrnkovich <mrcrnkovich@gmail.com>
2021-10-30 11:50:37 -0400

    moved config options for PDFKIT and Reports out of the main.py file into a config.yml file

--- main.py
+++ main.py
@@ -4,13 +4,10 @@ import pdfkit
 import jinja2
 import requests
 import sys
-import yaml
-from functools import partial
+from yaml import safe_load
+# from functools import partial
 
-
-pdf_options = {
-    'encoding':    'UTF-8'
-}
+config_filepath = './config.yml'
 
 def trim(path):
     return path.strip('./').strip('.py')
@@ -22,25 +19,33 @@ def get_data(path):
     return requests.get(path).json()
 
 def main():
-    # set up environment
+    # set up Jinja environment
     env = jinja2.Environment(
             loader=jinja2.PackageLoader("src"),
             autoescape=jinja2.select_autoescape()
     )
 
-    # Should pull config from yaml file
-    base_url = 'http://localhost:3000/'
-    
+    # Load template configurations
+    with open (config_filepath, 'r') as f:
+        config = safe_load(f)
+
+    pdf_config = config['PDF']
+    template_config = config['Templates']
+    print(pdf_config)
 
     report = trim(sys.argv[1])
-    template_name = report + '.html'
-    report_url = base_url + report
 
-    temp = env.get_template(template_name)
-    html = temp.render(name = "mike",
+    template_name = config['Templates'][report]['template_loc']
+    report_url = config['Templates'][report]['end_point']
+
+    template = env.get_template(template_name)
+    html = template.render(name = "mike",
             report=report,
             data=get_data(report_url))
-    pdfkit.from_string(html, pdf_filename(report), options=pdf_options)
+
+    pdfkit.from_string(html,
+            pdf_filename(report),
+            options=pdf_config)
 
 if __name__ == "__main__":
     main()
