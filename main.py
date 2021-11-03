ef1ed7f358d36e742cee6b875e966bac206e645e
mrcrnkovich <mrcrnkovich@gmail.com>
2021-11-03 10:13:32 -0400

    updated main.py to use yaml configs

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import jinja2
 import requests
 import sys
 from yaml import safe_load
-# from functools import partial
 
 config_filepath = './config.yml'
 
@@ -19,33 +18,34 @@ def get_data(path):
     return requests.get(path).json()
 
 def main():
+    
+    # Load commandline args
+    report = trim(sys.argv[1])
+
+    
+    # Load template configurations
+    with open (config_filepath, 'r') as f:
+        config = safe_load(f)
+
+    template_config = config.get('Templates').get(report)
+    template_name = template_config.get('template_loc')
+    report_url = template_config.get('end_point')
+
+
     # set up Jinja environment
     env = jinja2.Environment(
             loader=jinja2.PackageLoader("src"),
             autoescape=jinja2.select_autoescape()
     )
-
-    # Load template configurations
-    with open (config_filepath, 'r') as f:
-        config = safe_load(f)
-
-    pdf_config = config['PDF']
-    template_config = config['Templates']
-    print(pdf_config)
-
-    report = trim(sys.argv[1])
-
-    template_name = config['Templates'][report]['template_loc']
-    report_url = config['Templates'][report]['end_point']
-
     template = env.get_template(template_name)
     html = template.render(name = "mike",
             report=report,
             data=get_data(report_url))
 
+    # Render to PDF
     pdfkit.from_string(html,
             pdf_filename(report),
-            options=pdf_config)
+            options=config.get('PDF'))
 
 if __name__ == "__main__":
     main()
