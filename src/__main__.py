65af9bcb3439121f4a5367e504dc6f4986556550
Mike Crnkovich <mrcrnkovich@gmail.com>
2021-12-05 16:11:57 -0500

    minor linting

--- src/__main__.py
+++ src/__main__.py
@@ -7,33 +7,30 @@ from yaml import safe_load
 from pdfkit import from_string
 
 
-def trim(path):
-    return path.strip('./').strip('.py')
-
-def pdf_filename(report, output_dir='./output'):
+def pdf_filename(report: str, output_dir: str = './output') -> str:
     return output_dir+'/'+report+'.pdf'
 
-def get_data(path):
+
+def get_data(path: str) -> dict:
+    '''Takes https path and returns results in a dict'''
     from requests import get
-    return get(path).json()
+    result = get(path)
+    return result.json() if result.status_code == '200' else raise AttributeError
+
 
 def main():
     logging.basicConfig(filename="./app.log", level=logging.INFO)
 
-
-    # Load commandline args
-    report = trim(argv[1])
-    
+    report = argv[1].strip('./').strip('.py')
     logging.info(f'opening file {report}')
-    # Load template configurations
+
     config_filepath = './config.yml'
-    with open (config_filepath, 'r') as f:
+    with open(config_filepath, 'r') as f:
         config = safe_load(f)
 
     template_config = config.get('Templates').get(report)
     template_name = template_config.get('template_loc')
     report_url = template_config.get('end_point')
-
 
     # set up Jinja environment
     env = jinja2.Environment(
@@ -41,7 +38,7 @@ def main():
             autoescape=jinja2.select_autoescape()
     )
     template = env.get_template(template_name)
-    html = template.render(name = "mike",
+    html = template.render(name="mike",
             report=report,
             data=get_data(report_url))
 
@@ -49,6 +46,7 @@ def main():
     from_string(html,
             pdf_filename(report),
             options=config.get('PDF'))
+
 
 if __name__ == "__main__":
     main()
