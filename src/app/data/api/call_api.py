6792b914feeaf33d06a5afe2a1230ac1092f4782
Mike Crnkovich <mrcrnkovich@gmail.com>
2022-01-07 12:40:24 -0500

    adjusting file structure

---
+++ src/app/data/api/call_api.py
@@ -0,0 +1,15 @@
+import logging
+
+
+def api_data(path: str) -> dict:
+    """Takes https path and returns results in a dict"""
+    from requests import get
+
+    logging.info(f"API PATH: {path}")
+    result = get(path)
+    print(result)
+
+    if result.status_code == 200:
+        return result.json()
+    else:
+        logging.ERROR(f"failed to get data at {path}. Returned: {result.status_code}")
