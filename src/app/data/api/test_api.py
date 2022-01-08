f0b61f881c7ddfacf5f1e1c5ff49451c4ae299cc
Mike Crnkovich <mrcrnkovich@gmail.com>
2022-01-08 12:10:00 -0500

    removed duplicates from restructing

--- src/app/data/api/test_api.py
+++
@@ -1,6 +0,0 @@
-""" Will Test the API calls """
-
-
-def test_call_api():
-    """ Will test api calls """
-    pass
--- src/app/data/sqlite/test_sqlite.py
+++
@@ -1,8 +0,0 @@
-"""
-Will perform a test against the sqlite3 call
-"""
-
-
-def test_sqlite_call():
-    '''testing'''
-    pass
