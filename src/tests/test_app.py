48c4798fe49322175496c42b4dcb585fe0b97f7a
Mike Crnkovich <mrcrnkovich@gmail.com>
2022-01-19 10:25:06 -0500

    Adding test file

---
+++ src/tests/test_app.py
@@ -0,0 +1,17 @@
+from unittest.mock import MagicMock
+import app
+import subprocess
+
+def test_dir_exists(tmp_path):
+    tmp_file = tmp_path / "test_dir"
+    tmp_file.mkdir()
+    
+    assert app.dir_exists(tmp_file) == tmp_file
+
+def test_check_dependencies():
+
+    subprocess.call = MagicMock(return_value = 1)
+    assert app.check_dependencies() == True
+
+    subprocess.call = MagicMock(return_value = 0)
+    assert app.check_dependencies() == False
